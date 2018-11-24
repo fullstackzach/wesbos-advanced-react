3883aa745870a552263ce44147b6243f9ffde44b
Zach Willard <zacharywillard@northwesternmutual.com>
2018-11-23 20:48:32 -0600

    video-4-end

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Sell = props =>
+  <div>
+    <p>Sell!</p>
+    <Link href='/'>
+      <a>
+      Home!
+    </a>
+    </Link>
+  </div>
+
+export default Sell
